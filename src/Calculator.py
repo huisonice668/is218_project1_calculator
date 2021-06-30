f5db55cafe769815fd72edd1f07c8284d2a5698e
zihui <zb4@gmail.com>
2021-06-30 13:49:28 -0400

    Feature 12: removed static methods

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,26 +1,4 @@
 import math
-def addition(x, y):
-    return x + y
-
-def subtraction(x, y):
-    x = int(x)
-    y = int(y)
-    return x - y
-
-def multiplication(x, y):
-    return x * y
-
-def division(x, y):
-    if y == 0:
-        raise ValueError("Can not divide by zero")
-    return x / y
-
-def squaring(x):
-    return x * x
-
-def squareRoots(x):
-    return math.sqrt(x)
-
 
 class Calculator:
 
@@ -28,19 +6,21 @@ class Calculator:
         pass
 
     def add(x, y):
-        return addition(x, y)
+        return x + y
 
     def minus(x, y):
-        return subtraction(x, y)
+        return x - y
 
     def multiply(x, y):
-        return multiplication(x, y)
+        return x * y
 
     def divide(x, y):
-        return division(x, y)
+        if y == 0:
+            raise ValueError("Can not divide by zero")
+        return x / y
 
     def square(x):
-        return squaring(x)
+        return x * x
 
     def squareRoot(x):
-        return squareRoots(x)
+        return math.sqrt(x)
