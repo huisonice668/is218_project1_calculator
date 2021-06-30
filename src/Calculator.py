46ac6d15f0e07cab171ebe02cd280605ec3e4717
zihui <zb4@gmail.com>
2021-06-30 13:30:12 -0400

    Feature 10: added static methods for each methods

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,27 +1,46 @@
 import math
+def addition(x, y):
+    return x + y
+
+def subtraction(x, y):
+    x = int(x)
+    y = int(y)
+    return x - y
+
+def multiplication(x, y):
+    return x * y
+
+def division(x, y):
+    if y == 0:
+        raise ValueError("Can not divide by zero")
+    return x / y
+
+def squaring(x):
+    return x * x
+
+def squareRoots(x):
+    return math.sqrt(x)
+
+
 class Calculator:
-    result = 0
 
     def _init_(self):
-        x = 2 + 2
-        self.result = x
+        pass
 
     def add(x, y):
-        return x + y
+        return addition(x, y)
 
     def minus(x, y):
-        return x - y
+        return subtraction(x, y)
 
     def multiply(x, y):
-        return x * y
+        return multiplication(x, y)
 
     def divide(x, y):
-        if y == 0:
-            raise ValueError("Can not divide by zero")
-        return x / y
+        return division(x, y)
 
     def square(x):
-        return x * x
+        return squaring(x)
 
     def squareRoot(x):
-        return math.sqrt(x)
+        return squareRoots(x)
