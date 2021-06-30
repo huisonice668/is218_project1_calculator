a7179761cd8645fb4072104e8ffda02fa852cb98
zihui <zb4@gmail.com>
2021-06-29 21:47:06 -0400

    Feature 2: added 'add, minus, multiply, divide' functions to Calculator

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,5 +1,21 @@
 class Calculator:
     result = 0
 
-    def __init__(self):
-        pass
+    def _init_(self):
+        x = 2 + 2
+        self.result = x
+
+    def add(x, y):
+        return x + y
+
+    def minus(x, y):
+        return x - y
+
+    def multiply(x, y):
+        return x * y
+
+    def divide(x, y):
+        if y == 0:
+            print("divendend can not be 0")
+        return x / y
+
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -2,10 +2,20 @@ import unittest
 from Calculator import Calculator
 
 class MyTestCase(unittest.TestCase):
+    '''''''''
     def test_instantiate_calculator(self):
         calculator = Calculator()
-        self.assertEqual(Calculator, Calculator)
+        self.assertIsInstance(calculator, Calculator)
+
+    def test_results_property_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.result, 4)
+    '''''''''
+
+    def test_add(self):
+        result = Calculator.add(10, 5)
+        self.assertEqual(result ,15)
 
 
-if __name__ == '__main__':
+if __name__ ==  '__main__':
     unittest.main()
